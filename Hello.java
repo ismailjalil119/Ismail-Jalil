8bdce79883b23767a9f8519e8599c47fb09ab2f7
ismailjalil119 <109107105+ismailjalil119@users.noreply.github.com>
2022-08-24 22:48:47 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/********************************************************************************************
+* Java Program Name 
+*********************************************************************************************
+* Student Name (first and last name) 
+* Data Created
+* Course Number and Section Number
+********************************************************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("<Ismail Jalil>");
+		System.out.println("<Hello.java>");
+		System.out.println("<Java/Lab 01>");
+		System.out.println("<08/24/2022>");
+		System.out.println("<CCMSC 255-004>");
+		System.out.println();
+	}
+}
